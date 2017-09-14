d7e7b5f266a3d0d5f3c95482f021e179f11b5121
zwheui <zwheui@gmail.com>
2017-09-14 10:54:34 +0800

    Finish page01

--- src/app/components/page01/page01.component.ts
+++ src/app/components/page01/page01.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpFetchService } from '../../services/http-fetch.service';
 
 @Component({
   selector: 'app-page01',
   templateUrl: './page01.component.html',
-  styleUrls: ['./page01.component.scss']
+  styleUrls: ['./page01.component.scss'],
+  providers: [HttpFetchService],
 })
 export class Page01Component implements OnInit {
   pageData;
-  isShowChk: boolean;
-  myText: string;
 
-  constructor() { }
+  constructor(
+    private _httpSrv: HttpFetchService
+  ) { }
 
   ngOnInit() {
-    this.isShowChk = true;
-    this.pageData = {
-      title: 'my Title'
-    };
-    this.myText = 'my Text';
+    this.loadPageData();
+    // this.pageData = {
+    //   title: 'my Title',
+    //   body: '<p>asdfasdf</p><p>zzzz</p>'
+    // };
   }
+
+  async loadPageData() {
+    this.pageData = await this._httpSrv.getJSON('page01');
+
+    // this.pageData = await this._httpSrv.getHttp('navItemList');
+  }
+
   onClick(obj2nd) {
     console.log(obj2nd);
   }
